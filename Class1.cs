30da6d509ffb94d000f7bb3b54b041be691fda72
AaeoO <362304@via.dk>
2026-09-06 20:25:54 +0200

    s

---
+++ Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace DnpAssignment2026;
+
+public class Class1
+{
+}
---
+++ Server/InMemoryRepositories/CommentInMemoryRepository.cs
@@ -0,0 +1,62 @@
+﻿using RepositoryContracts;
+using Entities;
+
+namespace InMemoryRepositories;
+
+public class CommentInMemoryRepository : ICommentRepository
+{
+    private List<Comment> comments;
+    
+    public Task<Comment> AddAsync(Comment comment)
+    {
+        comment.Id = comments.Any()
+            ? comments.Max(c => c.Id) + 1
+            : 1;
+        comments.Add(comment);
+        return Task.FromResult(comment);
+    }
+    
+    public Task UpdateAsync(Comment comment)
+    {
+        Comment? existingComment = comments.SingleOrDefault(c => c.Id == comment.Id);
+        if (existingComment is null)
+        {
+            throw new InvalidOperationException(
+                $"Post with ID '{comment.Id}' not found");
+        }
+
+        comments.Remove(existingComment);
+        comments.Add(comment);
+
+        return Task.CompletedTask;
+    }
+    
+    public Task DeleteAsync(int id)
+    {
+        Comment? commentToRemove = comments.SingleOrDefault(c => c.Id == id);
+        if (commentToRemove is null)
+        {
+            throw new InvalidOperationException(
+                $"Post with ID '{id}' not found");
+        }
+
+        comments.Remove(commentToRemove);
+        return Task.CompletedTask;
+    }
+    
+    public Task<Comment> GetSingleAsync(int id)
+    {
+        Comment?  comment = comments.SingleOrDefault(c => c.Id == id);
+        if (comment is null)
+        {
+            throw new InvalidOperationException(
+                $"Post with ID '{id}' not found");
+        }
+        return Task.FromResult(comment);
+    }
+    
+    public IQueryable<Comment> GetMany()
+    {
+        return comments.AsQueryable();
+    }
+}
