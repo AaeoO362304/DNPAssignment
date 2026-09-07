d08bc9f0b597549f14e22e2396af60928be341c5
AaeoO <362304@via.dk>
2026-09-07 11:03:42 +0200

    d

---
+++ Server/CLI/Program.cs
@@ -0,0 +1,11 @@
+﻿using CLI.UI;
+using InMemoryRepositories;
+using RepositoryContracts;
+
+Console.WriteLine("Start CLI App..");
+IUserRepository userRepository = new UserInMemoryRepository();
+ICommentRepository commentRepository = new CommentInMemoryRepository();
+IPostRepository postRepository = new PostInMemoryRepository();
+
+CliApp cliApp = new CliApp(userRepository, commentRepository, postRepository);
+await cliApp.StartAsync();
