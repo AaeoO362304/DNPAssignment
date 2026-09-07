ddac2e7d818fccdca798e203749b0de807292af5
AaeoO <362304@via.dk>
2026-09-07 12:05:59 +0200

    s

--- Server/CLI/UI/ManagePosts/CreatePostView.cs
+++ Server/CLI/UI/ManagePosts/CreatePostView.cs
@@ -14,12 +14,15 @@ public class CreatePostView
 
     public void CreatePost()
     {
+        Console.Write("What is your user id?: ");
+        int userId = int.Parse(Console.ReadLine());
+        Console.WriteLine();
         Console.Write("Title: "); 
         String title = Console.ReadLine();
         Console.WriteLine();
         Console.Write("Body: "); 
         String body = Console.ReadLine();
-        Post post = new Post(title, body, 1);
+        Post post = new Post(title, body, userId);
         postRepository.AddAsync(post);
     }
 }
